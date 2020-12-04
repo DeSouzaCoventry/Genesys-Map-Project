5ba08402eb711888f7267f0bbe841a47c898382c
Ben DeSouza <32418260+DeSouzaCoventry@users.noreply.github.com>
2020-12-04 17:19:24 +0000

    Delete AppConstants.cs

--- AppConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace GoogleMaps.Constants
-{
-    public class AppConstants
-    {
-        public const string GoogleMapsApiKey = "AIzaSyC0XsANI91v_zAqhnwfKDdl_EwN5WFqgr4";
-    }
-}
