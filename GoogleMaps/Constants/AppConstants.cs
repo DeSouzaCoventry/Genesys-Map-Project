81d6f6f4b2fe9ab88297747cb85b6469c8569799
Ben DeSouza <32418260+DeSouzaCoventry@users.noreply.github.com>
2020-12-04 17:21:38 +0000

    Add files via upload

---
+++ GoogleMaps/Constants/AppConstants.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace GoogleMaps.Constants
+{
+    public class AppConstants
+    {
+        public const string GoogleMapsApiKey = "AIzaSyC0XsANI91v_zAqhnwfKDdl_EwN5WFqgr4";
+    }
+}
